4b3ae2d3a54eed9e008895df8e686791a5843c8e
leonardoandrade020896-eng <leonardo.andrade020896@gmail.com>
2026-08-12 22:22:59 -0300

    aula 12-08-26

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 # Instancia do servidor do Flask
 app = Flask(__name__)
@@ -6,17 +6,21 @@ app = Flask(__name__)
 #Rota 1: Pagina inicial
 @app.route('/')
 def home():
-    return "<h1>Servidor Flask rodando</h1>" "<h1>Bem-vindo ao meu servidor Flask!</h1>"
+    return render_template("index.html")
 
-#Rota 2: Sobre a aplicação
-@app.route('/sobre')
-def sobre():
-    return "<h1>Sobre a função</h1>" "<p>Esta é uma simples aplicação Flask.</p>"
+#Rota 2: Exibição da tela de cadastro Método (GET)
+@app.route('/cadastro')
+def pagina_cadastro():
+    return render_template("cadastro.html")
 
-#Rota 3:Status da aplicação
-@app.route('/status')
-def status():
-    return "<h1>Status da aplicação</h1>" "<p>O servidor Flask está rodando corretamente.</p>"
+#Rota 3:Processamento dos dados Método (POST)
+@app.route('/salvar', methods=["POST"])
+def salvar_cadastro():
+    nome_digitado = request.form.get("campo_nome")
+    info_digitado = request.form.get("campo_info")
+
+    return render_template("resultado.html", nome=nome_digitado, info=info_digitado)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
